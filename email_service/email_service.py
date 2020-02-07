30d8923c96b88d7901c76c5cab1ebe39c9b6d54a
Po Tong <potong616@gmail.com>
2020-02-06 23:07:22 -0800

    _EmailAccount_ should be done for now

--- email_service/email_service.py
+++ email_service/email_service.py
@@ -1,3 +1,6 @@
+import re
+
+
 class EmailAccount(object):
     ''' A class to represent an email account
 
@@ -6,15 +9,34 @@ class EmailAccount(object):
     email_address : str
         email address associated with the email account
 
-    naame : str
+    name : str
         display name of the email account
 
+    Raises:
+        ValueError: If email_address is not of proper format or name is missing
     '''
-    def __init__(self, email_address: str, name:str) -> None:
-        pass
+    def __init__(self, email_address: str, name: str) -> None:
+        email_address = email_address.strip()
+        if not re.fullmatch(r'[^@]+@[^@]+\.[^@]+', email_address):
+            raise ValueError('Invalid email address format')
+
+        self.email_address = email_address
+        name = name.strip()
+        if name == '':
+            raise ValueError('Missing name')
+
+        self.name = name
 
 
 class EmailService(object):
+    ''' A class to pergorm email sending operation
+
+    Attributes
+    ----------
+    external_api
+        api that does the actual email sending
+
+    '''
     def __init__(self, external_api) -> None:
         self.external_api = external_api
 
